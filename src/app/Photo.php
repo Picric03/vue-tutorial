f0db6603db734e3254a2a5f4c2c1087f620624bc
t.kakiuchi@moak1 <t.kakiuchi@moak1.co.jp>
2019-12-10 10:07:24 +0900

    chapter9

---
+++ src/app/Photo.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $keyType = 'string';
+
+    private const ID_LENGTH = 12;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (!array_get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    private function setId(): void
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    private function getRandomId(): string
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = '';
+
+        for ($i = 0; $i < self::ID_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
+}
---
+++ src/tests/Feature/PhotoSubmitApiTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use App\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PhotoSubmitApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function should_ファイルをアップロードできる(): void
+    {
+        Storage::fake('public');
+
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('photo.create'), [
+                'photo' => UploadedFile::fake()->image('photo.jpg'),
+            ]);
+
+        $response->assertStatus(201);
+
+        $photo = Photo::first();
+
+        $this->assertRegExp('/^[0-9a-zA-Z-_]{12}$/', $photo->id);
+        Storage::disk('public')->assertExists($photo->filename);
+    }
+
+    /**
+     * @test
+     */
+    public function should_データベースエラーの場合はファイルを保存しない(): void
+    {
+        Schema::drop('photos');
+
+        Storage::fake('public');
+
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('photo.create'),[
+                'photo' => UploadedFile::fake()->image('photo.jpg'),
+            ]);
+
+        $response->assertStatus(500);
+
+        $this->assertCount(0, Storage::disk('public')->files());
+    }
+
+    /**
+     * @test
+     */
+    public function should_ファイル保存エラーの場合はDBへの挿入はしない(): void
+    {
+        Storage::shouldReceive('disk')
+            ->once()
+            ->andReturnNull();
+
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('photo.create'), [
+                'photo' => UploadedFile::fake()->image('photo.jpg'),
+            ]);
+
+        $response->assertStatus(500);
+        $this->assertEmpty(Photo::all());
+    }
+}
